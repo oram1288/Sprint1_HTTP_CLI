04cf7ec4d1dbadfc01f756a37d3eb7149c0d90a3
oram1288 <cody.oram@keyin.com>
2024-10-28 12:19:16 -0230

    commit

---
+++ src/main/java/com/keyin/domain/Airport.java
@@ -0,0 +1,61 @@
+package com.keyin.domain;
+
+
+import java.util.Objects;
+
+public class Airport {
+
+    private long id;
+    private String name;
+    private String code;
+
+    public Airport() {
+    }
+
+    public Airport(String code) {
+        this.code = code;
+    }
+
+    public Airport(long id, String name, String code) {
+        this.id = id;
+        this.name = name;
+        this.code = code;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Airport airport = (Airport) o;
+        return Objects.equals(code, airport.code);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code);
+    }
+}
