5c717409d6b8fbbbbecb43b88e462dea56c6a037
oram1288 <cody.oram@keyin.com>
2024-11-08 14:20:54 -0330

    Final fixes

--- src/main/java/com/keyin/http/client/RESTClient.java
+++ src/main/java/com/keyin/http/client/RESTClient.java
@@ -22,39 +22,42 @@ public class RESTClient {
     private String serverURL;
     private HttpClient client;
 
-    public String getResponseFromHTTPRequest() {
-        String responseBody = "";
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(serverURL)).build();
+    public String getServerURL() {
+        return serverURL;
+    }
 
-        try {
-            HttpResponse<String> response = getClient().send(request, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode()!=200) {
-                System.out.println("Status Code: " + response.statusCode());
-            }
+    public void setServerURL(String serverURL) {
+        this.serverURL = serverURL;
+    }
 
-            responseBody = response.body();
-
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
+    public HttpClient getClient() {
+        if (client == null) {
+            client  = HttpClient.newHttpClient();
         }
 
-        return responseBody;
+        return client;
     }
+
+    private HttpResponse<String> httpSender(HttpRequest request) throws IOException, InterruptedException {
+        HttpResponse<String> response = getClient().send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode()==200) {
+            System.out.println("*****Response Body Print****");
+            System.out.println("***** " + response.body());
+        } else {
+            System.out.println("Error Status Code: " + response.statusCode());
+        }
+        return response;
+    }
+
 
     // Airport
     public List<Airport> getAllAirports() {
-        List<Airport> airports = new ArrayList<Airport>();
+        List<Airport> airports = new ArrayList<>();
 
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(serverURL)).build();
 
         try {
-            HttpResponse<String> response = getClient().send(request, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode()==200) {
-                System.out.println("***** " + response.body());
-            } else {
-                System.out.println("Error Status Code: " + response.statusCode());
-            }
-
+            HttpResponse<String> response = httpSender(request);
             airports = buildAirportListFromResponse(response.body());
 
         } catch (IOException | InterruptedException e) {
@@ -65,7 +68,7 @@ public class RESTClient {
     }
 
     public List<Airport> buildAirportListFromResponse(String response) throws JsonProcessingException {
-        List<Airport> airports = new ArrayList<Airport>();
+        List<Airport> airports = new ArrayList<>();
 
         ObjectMapper mapper = new ObjectMapper();
         mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
@@ -81,13 +84,7 @@ public class RESTClient {
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(serverURL)).build();
 
         try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode() == 200) {
-                System.out.println("***** " + response.body());
-                cities = buildCityListFromResponse(response.body());
-            } else {
-                System.out.println("Error Status Code: " + response.statusCode());
-            }
+            HttpResponse<String> response = httpSender(request);
 
             cities = buildCityListFromResponse(response.body());
 
@@ -115,13 +112,7 @@ public class RESTClient {
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(serverURL)).build();
 
         try{
-            HttpResponse<String> repsonse = client.send(request, HttpResponse.BodyHandlers.ofString());
-            if (repsonse.statusCode()==200) {
-                System.out.println("***** " + repsonse.body());
-                passengers = buildPassengerListFromResponse(repsonse.body());
-            } else{
-                System.out.println("Error Status Code: " + repsonse.statusCode());
-            }
+            HttpResponse<String> repsonse = httpSender(request);
 
             passengers = buildPassengerListFromResponse(repsonse.body());
         }catch (IOException | InterruptedException e){
@@ -145,13 +136,7 @@ public class RESTClient {
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(serverURL)).build();
 
         try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode() == 200) {
-                System.out.println("***** " + response.body());
-                aircrafts = buildAircraftListFromResponse(response.body());
-            } else {
-                System.out.println("Error Status Code: " + response.statusCode());
-            }
+            HttpResponse<String> response = httpSender(request);
 
             aircrafts = buildAircraftListFromResponse(response.body());
 
@@ -172,19 +157,4 @@ public class RESTClient {
         return aircrafts;
     }
 
-    public String getServerURL() {
-        return serverURL;
-    }
-
-    public void setServerURL(String serverURL) {
-        this.serverURL = serverURL;
-    }
-
-    public HttpClient getClient() {
-        if (client == null) {
-            client  = HttpClient.newHttpClient();
-        }
-
-        return client;
-    }
 }
